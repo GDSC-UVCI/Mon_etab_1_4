8d4cebecda745162ba90e1cbb3f25e7631ccec28
GDSC-UVCI <el.nuhu@uvci.edu.ci>
2024-08-25 16:45:59 +0000

    front added

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-
+#simple comment to change commit msg
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
